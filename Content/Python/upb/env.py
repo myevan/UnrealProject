186db1ca32c8d78ffb871fbd2e9f3c7759bb36de
Song Young-Jin <myevan@outlook.com>
2021-03-01 02:27:30 +0900

    Python: upb.env

---
+++ Content/Python/upb/env.py
@@ -0,0 +1,35 @@
+import unreal
+import os
+import pb
+
+class Environment(pb.Singleton):
+    def __init__(self):
+        self.__prj_file_path = ""
+        self.__prj_dir_path = ""
+        self.__eng_dir_path = ""
+        self.__svn_exe_path = "svn"
+
+    @property
+    def project_file_path(self):
+        if not self.__prj_file_path:
+            self.__prj_file_path = os.path.realpath(unreal.Paths.get_project_file_path())
+
+        return self.__prj_file_path
+    
+    @property
+    def project_dir_path(self):
+        if not self.__prj_dir_path:
+            self.__prj_dir_path = os.path.dirname(self.project_file_path)
+
+        return self.__prj_dir_path
+
+    @property
+    def engine_dir_path(self):
+        if not self.__eng_dir_path:
+            self.__eng_dir_path = os.path.realpath(unreal.Paths.root_dir())
+
+        return self.__eng_dir_path
+
+    def join_engine_path(self, *args):
+        return os.path.join(self.engine_dir_path, *args)
+
