1d73d9558086f0b0047fb197ba63b5f80f106800
Song Young-Jin <myevan@outlook.com>
2021-06-27 21:33:03 +0900

    Python: pip install target changed to pb.platforms

--- Content/Python/pip_install.bat
+++ Content/Python/pip_install.bat
@@ -1,3 +1,3 @@
 @echo off
 call %~dp0..\..\Build\BatchFiles\UE4Environ.bat || exit /b 1
-%UE_DIR%\Engine\Binaries\ThirdParty\Python3\Win64\python.exe -m pip install -t %~dp0 %*
+%UE_DIR%\Engine\Binaries\ThirdParty\Python3\Win64\python.exe -m pip install -t %~dp0\pb\platforms\Python-3.7.7-Windows %*
