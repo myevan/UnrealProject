9a8f5fccb838d5f64c3aaec68c76b34f9b25ab38
myevan <myevan@outlook.com>
2021-06-29 02:57:17 +0900

    DBC: dbc8, dbci, dbcu

--- Content/Python/pb/tools/dbc_tool.py
+++ Content/Python/pb/tools/dbc_tool.py
@@ -1,4 +1,3 @@
-from typing import OrderedDict
 from collections import OrderedDict
 
 import os
@@ -85,43 +84,106 @@ class Table:
 
         return Table(self.heads, [list(gen_casted_values(row)) for row in self.rows], parent=self)
 
-    def dump(self, rec_len, blk_idx, encoding='utf-8'):
+    def dump8(self, rec_len, blk_idx, sep=b'\0'):
         def gen_bytes(rows):
             blk_idx_bytes = bytes(ctypes.c_int16(blk_idx))
             rec_cnt = len(rows)
 
-            yield b'XDBC'
+            yield b'DBC8'
             yield bytes(ctypes.c_uint32(rec_cnt))            
             yield bytes(ctypes.c_uint16(rec_len))
             yield blk_idx_bytes            
 
-            strs = []
-            str_off = 0            
+            utf8_strs = []
+            utf8_off = 0
+
             for irow, vals in enumerate(rows):
                 for icol, val in enumerate(vals):
                     if type(val) is str:
-                        str_len = len(val)
-                        yield bytes(ctypes.c_int16(str_len))
+                        utf8_str = val.encode('utf-8')
+                        utf8_len = len(utf8_str)
+                        yield bytes(ctypes.c_int16(utf8_len))
                         yield blk_idx_bytes                        
-                        yield bytes(ctypes.c_int32(str_off))
+                        yield bytes(ctypes.c_int32(utf8_off))
 
-                        strs.append(val)
-                        strs.append('\0')
-                        str_off += str_len + 1
+                        utf8_strs.append(utf8_str + sep)
+                        utf8_off += utf8_len + len(sep)
                     else:
                         yield bytes(val)
 
-            total_str = ''.join(strs)
-            yield total_str.encode(encoding)
+            yield b''.join(utf8_strs)
         
         return b''.join(gen_bytes(self.rows))
-                
+
+    def dumpu(self, rec_len, blk_idx, sep=b'\t'):
+        def gen_bytes(rows):
+            blk_idx_bytes = bytes(ctypes.c_int16(blk_idx))
+            rec_cnt = len(rows)
+
+            yield b'DBCU'
+            yield bytes(ctypes.c_uint32(rec_cnt))            
+            yield bytes(ctypes.c_uint16(rec_len))
+            yield blk_idx_bytes            
+
+            utf8_strs = []
+            uni_off = 0
+            uni_sep = sep.decode('utf-8')
+
+            for irow, vals in enumerate(rows):
+                for icol, val in enumerate(vals):
+                    if type(val) is str:
+                        uni_len = len(val)
+
+                        yield bytes(ctypes.c_int16(uni_len))
+                        yield blk_idx_bytes                        
+                        yield bytes(ctypes.c_int32(uni_off))
+
+                        utf8_strs.append(val.encode('utf8') + sep)
+                        uni_off += len(val) + len(uni_sep)
+                    else:
+                        yield bytes(val)
+
+            yield b''.join(utf8_strs)
+
+        return b''.join(gen_bytes(self.rows))
+
+    def dumpi(self, rec_len, blk_idx, sep=b'\t'):
+        def gen_bytes(rows, uni_strs):
+            blk_idx_bytes = bytes(ctypes.c_int16(blk_idx))
+            rec_cnt = len(rows)
+
+            yield b'DBCI'
+            yield bytes(ctypes.c_uint32(rec_cnt))            
+            yield bytes(ctypes.c_uint16(rec_len))
+
+            uni_sep = sep.decode('utf-8')
+
+            for irow, vals in enumerate(rows):
+                for icol, val in enumerate(vals):
+                    if type(val) is str:
+                        uni_str = val
+                        uni_str_len = len(uni_str)
+                        uni_str_idx = len(uni_strs)
+
+                        yield bytes(ctypes.c_int16(uni_str_len))
+                        yield blk_idx_bytes                        
+                        yield bytes(ctypes.c_int32(uni_str_idx))
+
+                        uni_strs.append(uni_str + uni_sep)
+                    else:
+                        yield bytes(val)
+
+        uni_strs = [] 
+        main_bytes = b''.join(gen_bytes(self.rows, uni_strs))
+        utf8_bytes = b''.join(uni_str.encode('utf8') for uni_str in uni_strs)
+        return (main_bytes, utf8_bytes)
+
 
 class CSVTable(Table):
     @classmethod
     def load(cls, csv_path):
         if os.path.isdir(csv_path):
-            pass
+            print(f"CSV_DIR: {csv_path}")
         else:
             csv_file_path = csv_path + '.csv'
             with open(csv_file_path) as csv_file:
@@ -148,6 +210,19 @@ class DBCTool:
             casted_table = selected_table.cast(field_c_types)
 
             rec_len = scheme.get_record_len()
-            dbc_bytes = casted_table.dump(rec_len, blk_idx=0)
-            with open(os.path.join(dbc_root_path, scheme.name + '.dbc'), 'wb') as dbc_file:
-                dbc_file.write(dbc_bytes)
+            dbc8_bytes = casted_table.dump8(rec_len, blk_idx=0)
+            with open(os.path.join(dbc_root_path, scheme.name + '.dbc8'), 'wb') as dbc8_file:
+                dbc8_file.write(dbc8_bytes)
+
+            dbcu_bytes = casted_table.dumpu(rec_len, blk_idx=0)
+            with open(os.path.join(dbc_root_path, scheme.name + '.dbcu'), 'wb') as dbcu_file:
+                dbcu_file.write(dbcu_bytes)
+
+            dbci_bytes, tsv_bytes = casted_table.dumpi(rec_len, blk_idx=0)
+            with open(os.path.join(dbc_root_path, scheme.name + '.dbci'), 'wb') as dbci_file:
+                dbci_file.write(dbci_bytes)
+            with open(os.path.join(dbc_root_path, scheme.name + '.tsv'), 'wb') as tsv_file:
+                tsv_file.write(tsv_bytes)
+
+if __name__ == '__main__':
+    DBCTool().build('.', '.')
